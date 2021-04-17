2bee32b7ccc7c8cad77eab8adf502d0f2e3ef814
Code Sandbox <hello@codesandbox.io>
2021-04-17 14:03:04 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5e6e8cebe6d00d257f75352cb6b9a2af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9209482ed162735b359e454eb49a61",
+    "url": "https://.github.io/csb-emrpx/index.html"
+  },
+  {
+    "revision": "4b1bf79b438c884652dd",
+    "url": "https://.github.io/csb-emrpx/static/css/main.bfd1e157.chunk.css"
+  },
+  {
+    "revision": "3cea431490733c20a53f",
+    "url": "https://.github.io/csb-emrpx/static/js/2.22e987b3.chunk.js"
+  },
+  {
+    "revision": "4b1bf79b438c884652dd",
+    "url": "https://.github.io/csb-emrpx/static/js/main.61c016a7.chunk.js"
+  },
+  {
+    "revision": "8ed9736bae460bb03718",
+    "url": "https://.github.io/csb-emrpx/static/js/runtime~main.bb8f574d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://.github.io/csb-emrpx/precache-manifest.5e6e8cebe6d00d257f75352cb6b9a2af.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://.github.io/csb-emrpx/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
